bc621e9e3f9260ff8ba7e56ae7dc7cdef159b51e
YUE <1311655997@qq.com>
2024-04-20 00:41:28 +0800

    每日推荐页面切换

---
+++ pages/daily/daily.vue
@@ -0,0 +1,94 @@
+<script setup>
+	import { ref } from 'vue'
+	// #ifdef H5
+	import recommend from '../../wxcomponents/recommend/recommend.vue'
+	// #endif
+	const title = ['每日推荐', '风格推荐']
+	const curIndex = ref(0)
+	const changeIndex = (index) => {
+		curIndex.value = index
+	}
+	const move = (index) => {
+		console.log(event.touches[0].clientX)
+		// if(event.touches[0].clientX > 0 && event.touches[0].clientX <= 300){
+		// 	curIndex.value = 1
+		// } else {
+		// 	curIndex.value = 0
+		// }
+	}
+</script>
+<template>
+	<view class="daily">
+		<uni-icons class="back" type="arrow-left" size="40" color="#ffffff"></uni-icons>
+		<view class="tab">
+			<view
+				class="tab-item"
+				v-for="(item, index) in title"
+				key="item"
+				:class="{active: curIndex === index}"
+				@click="curIndex = index"
+				:current="curIndex">
+				<text>{{item}}</text>
+			</view>
+		</view>
+		<swiper class="swiper-box" :current="curIndex">
+			<swiper-item
+				v-for="(item, index) in title"
+				:key="item"
+				@change="curIndex = index"
+				@touchmove="move(index)"
+			>
+				<view class="swiper-item" :class="'swiper-item' + index">
+					<recommend name="每日推荐"></recommend>
+				</view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+<style lang="scss">
+	.daily{
+		width: 100vw;
+		height: 100vh;
+		position: relative;
+		.back{
+			position: absolute;
+			top: 20rpx;
+			left: 18rpx;
+			z-index: 1;
+		}
+		.tab{
+			height: 70rpx;
+			width: 428rpx;
+			background: rgba(0, 0, 0, .1);
+			position: fixed;
+			top: 20rpx;
+			left: 50%;
+			transform: translate(-50%);
+			border-radius: 80rpx;
+			z-index: 1;
+			display: flex;
+			line-height: 70rpx;
+			padding: 8rpx;
+			.tab-item{
+				flex: 1;
+				text-align: center;
+				height: 100%;
+				border-radius: 72rpx;
+				color: #ffffff;
+				&.active{
+					background: #ffffff;
+					color: black;
+				}
+			}
+		}
+	}
+	.swiper-box {
+		height: 100vh;
+	}
+	.swiper-item {
+		height: 100%;
+	}
+
+</style>
+
+
---
+++ unpackage/dist/dev/mp-weixin/pages/daily/daily.js
@@ -0,0 +1,53 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _component_recommend = common_vendor.resolveComponent("recommend");
+  (_easycom_uni_icons2 + _component_recommend)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  _easycom_uni_icons();
+}
+const _sfc_main = {
+  __name: "daily",
+  setup(__props) {
+    const title = ["每日推荐", "风格推荐"];
+    const curIndex = common_vendor.ref(0);
+    const move = (index) => {
+      console.log(event.touches[0].clientX);
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.p({
+          type: "arrow-left",
+          size: "40",
+          color: "#ffffff"
+        }),
+        b: common_vendor.f(title, (item, index, i0) => {
+          return {
+            a: common_vendor.t(item),
+            b: curIndex.value === index ? 1 : "",
+            c: common_vendor.o(($event) => curIndex.value = index)
+          };
+        }),
+        c: curIndex.value,
+        d: common_vendor.f(title, (item, index, i0) => {
+          return {
+            a: "48d80d29-1-" + i0,
+            b: common_vendor.n("swiper-item" + index),
+            c: item,
+            d: common_vendor.o(($event) => curIndex.value = index, item),
+            e: common_vendor.o(($event) => move(), item)
+          };
+        }),
+        e: common_vendor.p({
+          name: "每日推荐"
+        }),
+        f: curIndex.value
+      };
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "C:/Users/13116/Desktop/BigProject/music163/pages/daily/daily.vue"]]);
+wx.createPage(MiniProgramPage);

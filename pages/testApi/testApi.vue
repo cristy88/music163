9ce74daafae36b13f65b6714443894bfceb71568
LYR <lyr04202022@163.com>
2024-04-20 17:02:16 +0800

    增加接口测试文件

---
+++ pages/testApi/testApi.vue
@@ -0,0 +1,23 @@
+<script setup>
+	import { getUserSongSheetApi, isLikeSongSheet } from '../../services/userApi.js'
+	const userId = uni.getStorageSync('id')
+	const testApi = async () => {
+		const res2 = await getUserSongSheetApi(userId)
+		console.log('获得用户歌单', res2)
+		const res3 = await isLikeSongSheet(userId)
+		console.log('相关歌单推荐', res3)
+	}
+	
+	testApi()
+	
+</script>
+
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<style lang="scss">
+
+</style>

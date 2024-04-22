dcc4cdae628303519f080300ca13e94d1025ef49
YUE <1311655997@qq.com>
2024-04-23 00:03:58 +0800

    player

--- pages/player/player.vue
+++ pages/player/player.vue
@@ -7,11 +7,18 @@
 
 	const id = ref('')
   const popup = ref(null)
+	const isShow = ref(true)
 	
-	const songDetail = async (id) => {
-
+	// 获取歌曲id
+	onLoad((option) => {
+		console.log("播放歌曲id",option)
+		id.value = option.id
+	})
+	
+	const songDetail = async (ids) => {
+		ids = id?.value
 		try {
-			const res = await getSongDetailApi(id)
+			const res = await getSongDetailApi(ids)
 			console.log("推荐",res)
 		} catch (e) {
 			console.log(e)
@@ -19,78 +26,77 @@
 	} 
 	songDetail()
 	
-	onLoad((option) => {
-		console.log("播放歌曲id",option)
-		id.value = option.id
-	})
 	
-	const open = () => {
-	        // 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
-	        popup.value.open('top')
+	// 展示隐藏于页面，跳至上一页
+	const changePage = () => {
+		console.log('显示隐藏页面', isShow.value)
+		isShow.value = !isShow.value
+		uni.navigateBack(-1)
 	}
 	
 </script>
 
 <template>
-	<view class="bg">
-		<view class="bg-color" style="backgroundImage: url(../../static/img/dailyback.jpg)"></view>
-	</view>
-	<view class="player">
-		<view class="header">
-			<!-- <view class=""> -->
-				<uni-icons class="down" type="right" size="28" color="#efefef"></uni-icons>
-			<!-- </view> -->
-			
-			<view class="tit">title</view>
-			<uni-icons class="redo" type="redo" size="28" color="#efefef"></uni-icons>
-		</view>
-		<view class="main">
-			<image class="needle" src="../../static/img/playneedle.png" mode="widthFix"></image>
-			<view class="circle">
-				<view class="circle-ring">
-					<view class="img">
-						<image src="../../static/img/dailyback.jpg"></image>
+	<view :class="[isShow ? ['animate__animated','animate__slideInUp'] : ['animate__animated','animate__slideOutDown']]">
+		<view class="playerPage" v-show="isShow">
+			<view class="bg">
+				<view class="bg-color" style="backgroundImage: url(../../static/img/dailyback.jpg)"></view>
+			</view>
+			<view class="player">
+				<view class="header">
+					<uni-icons class="down" type="right" size="28" color="#efefef" @click="changePage"></uni-icons>
+					<view class="tit">title</view>
+					<uni-icons class="redo" type="redo" size="28" color="#efefef"></uni-icons>
+				</view>
+				<view class="main">
+					<image class="needle" src="../../static/img/playneedle.png" mode="widthFix"></image>
+					<view class="circle">
+						<view class="circle-ring">
+							<view class="img">
+								<image src="../../static/img/dailyback.jpg"></image>
+							</view>
+						</view>
 					</view>
 				</view>
-			</view>
-		</view>
-		<view class="footer">
-			<!-- 歌名 -->
-			<view class="songTitle">
-				<view class="songName">
-					<view class="song">
-						吹梦到西洲
+				<view class="footer">
+					<!-- 歌名 -->
+					<view class="songTitle">
+						<view class="songName">
+							<view class="song">
+								吹梦到西洲
+							</view>
+							<view class="singer">
+								妖扬
+							</view>
+						</view>
+						<uni-icons class="like" type="heart" size="26" color="#ffffff"></uni-icons>
+						<uni-icons class="review" type="chat" size="28" color="#ffffff"></uni-icons>
 					</view>
-					<view class="singer">
-						妖扬
+					<!-- 进度条 -->
+					<view class="control">
+						<slider class="slider" @change="" block-size="8" active-color="#ffffff"/>
+						<view class="time">
+							<view class="begin">
+								00:00
+							</view>
+							<view class="end">
+								00:00
+							</view>
+						</view>
+					</view>
+					<!-- 按键 -->
+					<view class="playBar">
+						<image class="xunhuan" src="../../static/suijibofang.png" mode="widthFix"></image>
+						<image class="shang" src="../../static/shang.png" mode="widthFix"></image>
+						<image class="bofang" src="../../static/bofang.png" mode="widthFix"></image>
+						<image class="xia" src="../../static/shang.png" mode="widthFix"></image>
+						<image class="caidan" src="../../static/caidan.png" mode="widthFix"></image>
+					</view>
+					<view class="info">
+						<image class="xiazai" src="../../static/xiazai.png" mode="widthFix"></image>
+						<uni-icons type="more-filled" size="24" color="#ffffff"></uni-icons>
 					</view>
 				</view>
-				<uni-icons class="like" type="heart" size="26" color="#ffffff"></uni-icons>
-				<uni-icons class="review" type="chat" size="28" color="#ffffff"></uni-icons>
-			</view>
-			<!-- 进度条 -->
-			<view class="control">
-				<slider class="slider" @change="" block-size="8" active-color="#ffffff"/>
-				<view class="time">
-					<view class="begin">
-						00:00
-					</view>
-					<view class="end">
-						00:00
-					</view>
-				</view>
-			</view>
-			<!-- 按键 -->
-			<view class="playBar">
-				<image class="xunhuan" src="../../static/suijibofang.png" mode="widthFix"></image>
-				<image class="shang" src="../../static/shang.png" mode="widthFix"></image>
-				<image class="bofang" src="../../static/bofang.png" mode="widthFix"></image>
-				<image class="xia" src="../../static/shang.png" mode="widthFix"></image>
-				<image class="caidan" src="../../static/caidan.png" mode="widthFix"></image>
-			</view>
-			<view class="info">
-				<image class="xiazai" src="../../static/xiazai.png" mode="widthFix"></image>
-				<uni-icons type="more-filled" size="24" color="#ffffff"></uni-icons>
 			</view>
 		</view>
 	</view>
@@ -98,6 +104,11 @@
 
 <style lang="scss" scoped>
 @import '../../uni.scss';
+
+.playerPage, .animate__animated{
+	width: 100%;
+	height: 100%;
+}
 .bg{
 	width: 100%;
 	height: 100%;

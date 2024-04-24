90816aab02747ad7d3c8e6d3cfb261a2be19d6b4
3136786440 <3136786440@qq.com>
2024-04-25 00:38:58 +0800

    测试

--- services/request.js
+++ services/request.js
@@ -1,6 +1,5 @@
 
 const BaseURL = 'https://zyxcl.xyz/music/api'
-const BaseURL = 'https://zyxcl.xyz/music/api'
 
 // 封装请求函数
 export const request = ({ url, method = 'GET', data = {}, header = {} }) => {
